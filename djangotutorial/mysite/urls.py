07361aec32b169b2db198ff170c64ed981b1d402
KunalGsolanki <kgs2002107@gmail.com>
2025-06-19 22:28:37 +0530

    update

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from vege.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("receipes/",receipes,name='receipes'),
     path("admin/", admin.site.urls),
 ]
+
--- djangotutorial/vege/views.py
+++ djangotutorial/vege/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def receipes(request):
+     if request.method == "POST":
+       data = request.POST
+       receipe_name = data.get("receipe_name")
+       receipe_description = data.get("receipe_description")
+       receipe_image = request.FILES.get("receipe_image")
+       Receipe.objects.create(
+           receipe_name=receipe_name,
+           receipe_description=receipe_description,
+           receipe_image=receipe_image
+       )
+       
      return render(request, "receipes.html")
