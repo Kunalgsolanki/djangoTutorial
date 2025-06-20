97619439095b3ee9b92f4cec76a1488e2dfd64e9
KunalGsolanki <kgs2002107@gmail.com>
2025-06-21 00:29:25 +0530

    add receipe app

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import include, path
 from vege.views import *
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("receipes/",receipes,name='receipes'),
+    path("delete_receipe/<id>",delete_receipe,name='delete_receipe'),
+    path("update_receipe/<id>",update_receipe,name='update_receipe'),
     path("admin/", admin.site.urls),
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
--- djangotutorial/vege/views.py
+++ djangotutorial/vege/views.py
@@ -1,16 +1,49 @@
-from django.shortcuts import render
-from .models import *
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Receipe
+from django.http import HttpResponse
+
 def receipes(request):
-     if request.method == "POST":
-       data = request.POST
-       receipe_name = data.get("receipe_name")
-       receipe_description = data.get("receipe_description")
-       receipe_image = request.FILES.get("receipe_image")
-       Receipe.objects.create(
-           receipe_name=receipe_name,
-           receipe_description=receipe_description,
-           receipe_image=receipe_image
-       )
-       
-     return render(request, "receipes.html")
+    if request.method == "POST":
+        data = request.POST
+        receipe_name = data.get("receipe_name")
+        receipe_description = data.get("receipe_description")
+        receipe_image = request.FILES.get("receipe_image")
+        
+        Receipe.objects.create(
+            receipe_name=receipe_name,
+            receipe_description=receipe_description,
+            receipe_image=receipe_image
+        )
+        return redirect("/receipes/")  
+    
+    # For GET request
+    queryset = Receipe.objects.all()
+
+    if request.GET.get('search'):
+        queryset=queryset.filter(receipe_name__icontains = request.GET.get('search'))
+    context = {'receipes': queryset}
+    return render(request, "receipes.html", context)
+def delete_receipe(request,id):
+    querySet = Receipe.objects.filter(id=id)
+    querySet.delete()
+    return redirect("/receipes/")
+def update_receipe(request,id):
+    querySet = Receipe.objects.get(id = id)
+    
+    if request.method =="POST":
+        data = request.POST
+        receipe_name = data.get("receipe_name")
+        receipe_description = data.get("receipe_description")
+        receipe_image = request.FILES.get("receipe_image")
+
+        querySet.receipe_name = receipe_name
+        querySet.receipe_description =receipe_description
+        
+        if receipe_image:
+             querySet.receipe_image = receipe_image
+    
+    
+        querySet.save()
+        return redirect("/receipes/")
+    context = {'receipes': querySet}
+    return render(request,"upadte_receipe.html", context)
