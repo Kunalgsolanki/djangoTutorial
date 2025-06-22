cb6ce2581ad859e530af07b3a2886b1eccc8bd66
KunalGsolanki <kgs2002107@gmail.com>
2025-06-22 16:59:56 +0530

    update

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -8,9 +8,13 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("receipes/",receipes,name='receipes'),
+    path("login/",login_page,name='login'),
+    path("register/",register_page,name='register'),
     path("delete_receipe/<id>",delete_receipe,name='delete_receipe'),
     path("update_receipe/<id>",update_receipe,name='update_receipe'),
     path("admin/", admin.site.urls),
+    path("logout/", logout_page,name="logout"),
+    path('profile/', user_details, name='user_details'),
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djangotutorial/vege/views.py
+++ djangotutorial/vege/views.py
@@ -1,49 +1,115 @@
 from django.shortcuts import render, redirect
 from .models import Receipe
 from django.http import HttpResponse
+from django.contrib.auth.models import  User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
+def user_details(request):
+    return render(request, "UserDetails.html")
 def receipes(request):
     if request.method == "POST":
         data = request.POST
         receipe_name = data.get("receipe_name")
         receipe_description = data.get("receipe_description")
         receipe_image = request.FILES.get("receipe_image")
-        
+
         Receipe.objects.create(
             receipe_name=receipe_name,
             receipe_description=receipe_description,
-            receipe_image=receipe_image
+            receipe_image=receipe_image,
+            user=request.user
         )
-        return redirect("/receipes/")  
-    
-    # For GET request
+        return redirect("/receipes/")
+
     queryset = Receipe.objects.all()
 
     if request.GET.get('search'):
-        queryset=queryset.filter(receipe_name__icontains = request.GET.get('search'))
+        queryset = queryset.filter(
+            receipe_name__icontains=request.GET.get('search'))
     context = {'receipes': queryset}
     return render(request, "receipes.html", context)
-def delete_receipe(request,id):
+
+
+def delete_receipe(request, id):
     querySet = Receipe.objects.filter(id=id)
     querySet.delete()
     return redirect("/receipes/")
-def update_receipe(request,id):
-    querySet = Receipe.objects.get(id = id)
-    
-    if request.method =="POST":
+
+
+def update_receipe(request, id):
+    querySet = Receipe.objects.get(id=id)
+
+    if request.method == "POST":
         data = request.POST
         receipe_name = data.get("receipe_name")
         receipe_description = data.get("receipe_description")
         receipe_image = request.FILES.get("receipe_image")
 
         querySet.receipe_name = receipe_name
-        querySet.receipe_description =receipe_description
-        
+        querySet.receipe_description = receipe_description
+
         if receipe_image:
-             querySet.receipe_image = receipe_image
-    
-    
+            querySet.receipe_image = receipe_image
+
         querySet.save()
         return redirect("/receipes/")
     context = {'receipes': querySet}
-    return render(request,"upadte_receipe.html", context)
+    return render(request, "upadte_receipe.html", context)
+
+def logout_page(request):
+    logout(request)
+    return redirect("/login/")
+def login_page(request):    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(password)
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, "Invalid Username and password")
+            return redirect('/login/')
+        user =authenticate(username = username,password = password)
+
+        if user is None:
+            messages.error(request, "Invalid  password")
+            return redirect('/login/')
+        
+        else:
+            login(request,user)
+            return redirect("/receipes/")
+
+    return render(request, "login.html")
+
+
+def register_page(request):
+
+    if request.method == "POST":
+        data = request.POST
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        username = data.get("username") 
+        password = data.get("password")
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.info(request, "UserName already taken")
+            return redirect('/register/')
+        user = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+        user.set_password(password)
+        user.save()
+
+        messages.info(request,"Account Created Succesfully !")
+
+        return redirect('/register/')
+        
+        
+
+    return render(request, "register.html")
